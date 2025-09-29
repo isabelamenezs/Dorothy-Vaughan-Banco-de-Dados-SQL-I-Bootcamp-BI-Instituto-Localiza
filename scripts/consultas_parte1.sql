a4f95fde578bc4a4049679955162611b96c8ed44
Alana Sampaio <alana.sampaio2000@gmail.com>
2025-09-29 18:59:01 -0300

    Inserção da questão 4 da consultas_parte1 onde é feito um filtro de data.

--- scripts/consultas_parte1.sql
+++ scripts/consultas_parte1.sql
@@ -1,7 +1,9 @@
 -- 1) 
 -- 2) 
 -- 3) 
--- 4) 
+-- 4) Filtro de Data: Mostre o nome e o email dos alunos que ingressaram a partir do início de 2023 (ou seja, DATA_INGRESSO maior ou igual a '2023-01- 01').
+SELECT NOME_ALUNO, EMAIL FROM ALUNOS WHERE DATA_INGRESSO >= '2023-01-01';
+
 -- 5) Filtro com AND e Ordenação: Liste o nome e a nota final de todas as matrículas do aluno ID_ALUNO = 1 e cuja NOTA_FINAL foi maior ou igual a 7.0. Ordene o resultado pela nota, da maior para menor.
 SELECT ALUNOS.NOME_ALUNO, MATRICULAS.NOTA_FINAL
 FROM ALUNOS, MATRICULAS
