dcb7422e488c434f944d3c1a64dae62b6ce8311d
laurianoalessa1-byte <lauriano.alessa1@gmail.com>
2025-09-28 21:14:06 -0300

    Update consultas_parte2.sql

--- scripts/consultas_parte2.sql
+++ scripts/consultas_parte2.sql
@@ -2,6 +2,14 @@
 SELECT DISTINCT c.DEPARTAMENTO
 FROM CURSOS c 
 
+  -- 2) Lógica Condicional: Relatório de matrículas com STATUS
+SELECT 
+    ID_MATRICULA,
+    CASE 
+        WHEN NOTA_FINAL >= 7.0 THEN 'Aprovado'
+        ELSE 'Reprovado'
+    END AS STATUS
+FROM MATRICULAS;
 
 -- 3) Ordenação e Limite: Qual é o curso com o maior número de créditos? O resultado deve mostrar apenas o nome do curso e seus créditos.
 SELECT * FROM CURSOS
