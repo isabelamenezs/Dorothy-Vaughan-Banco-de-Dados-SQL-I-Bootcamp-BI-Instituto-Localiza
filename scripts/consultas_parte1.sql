e81a8cb679806cecb1e8f4926b727dc51528bd8b
Dani <94472881+suri0702@users.noreply.github.com>
2025-09-29 08:17:03 -0300

    Update consultas_parte1.sql
    Item 5 adicionado da parte 1 do exercício para entrega

--- scripts/consultas_parte1.sql
+++ scripts/consultas_parte1.sql
@@ -1,1 +1,9 @@
-
+-- 1) 
+-- 2) 
+-- 3) 
+-- 4) 
+-- 5) Filtro com AND e Ordenação: Liste o nome e a nota final de todas as matrículas do aluno ID_ALUNO = 1 e cuja NOTA_FINAL foi maior ou igual a 7.0. Ordene o resultado pela nota, da maior para menor.
+SELECT ALUNOS.NOME_ALUNO, MATRICULAS.NOTA_FINAL
+FROM ALUNOS, MATRICULAS
+WHERE ALUNOS.ID_ALUNO = 1 AND MATRICULAS.NOTA_FINAL >= 7.0
+ORDER BY MATRICULAS.NOTA_FINAL DESC 
