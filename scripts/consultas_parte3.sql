c948709b914067cfca03b896a62ca98e74003ba9
Ana Beatriz Costa <66637302+AnaBeCosta@users.noreply.github.com>
2025-10-04 19:14:36 -0300

    Adicionando desafio em pdf e minha parte do exercício

--- scripts/consultas_parte3.sql
+++ scripts/consultas_parte3.sql
@@ -1,1 +1,15 @@
+-- 1. Contagem e Média: Calcule o número total de matrículas e a média geral de todas as notas finais.
+
+
+-- 2. Agrupamento: Crie um relatório que mostre quantos cursos são oferecidos por departamento. ✅
+
+SELECT 
+    DEPARTAMENTO AS "Departamento",
+    COUNT(ID_CURSO) AS "Quantidade de Cursos"
+FROM 
+    CURSOS
+GROUP BY 
+    DEPARTAMENTO
+ORDER BY 
+    COUNT(ID_CURSO) DESC;
 
