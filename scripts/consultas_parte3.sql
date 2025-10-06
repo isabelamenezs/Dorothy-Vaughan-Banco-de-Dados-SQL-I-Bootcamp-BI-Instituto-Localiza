ecc5d2e463a76448f59f5c914d32d20e8f7db223
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-06 10:39:47 -0300

    Update consultas_parte3.sql
    Adição do código da Nicoli 3.2 no arquivo padrão do repositório.

--- scripts/consultas_parte3.sql
+++ scripts/consultas_parte3.sql
@@ -2,7 +2,6 @@
 
 
 -- 2. Agrupamento: Crie um relatório que mostre quantos cursos são oferecidos por departamento. ✅
-
 SELECT 
     DEPARTAMENTO AS "Departamento",
     COUNT(ID_CURSO) AS "Quantidade de Cursos"
@@ -12,4 +11,12 @@ GROUP BY
     DEPARTAMENTO
 ORDER BY 
     COUNT(ID_CURSO) DESC;
+
+-- 3. Filtro de Grupo (departamentos com mais de um curso)
+SELECT 
+    DEPARTAMENTO AS "Departamento Responsável",
+    COUNT(*) AS "Quantidade de Cursos"
+FROM CURSOS
+GROUP BY DEPARTAMENTO
+HAVING COUNT(*) > 1;
 
