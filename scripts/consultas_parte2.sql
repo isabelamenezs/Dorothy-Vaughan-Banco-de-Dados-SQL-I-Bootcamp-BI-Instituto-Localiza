c461ae5ede139f4ac4994d21304786f3426bba2a
Isabela Menezes <56889329+isabelamenezs@users.noreply.github.com>
2025-10-06 10:36:54 -0300

    Update consultas_parte2.sql
    Ajustes de pontuação

--- scripts/consultas_parte2.sql
+++ scripts/consultas_parte2.sql
@@ -1,6 +1,6 @@
 -- 1) Valores Únicos: Qual é a lista de departamentos únicos que oferecem cursos na universidade?
 SELECT DISTINCT c.DEPARTAMENTO
-FROM CURSOS c 
+FROM CURSOS c; 
 
   -- 2) Lógica Condicional: Relatório de matrículas com STATUS
 SELECT 
@@ -12,8 +12,6 @@ SELECT
 FROM MATRICULAS;
 
 -- 3) Ordenação e Limite: Qual é o curso com o maior número de créditos? O resultado deve mostrar apenas o nome do curso e seus créditos.
-SELECT * FROM CURSOS
-
 SELECT NOME_CURSO, CURSOS.CREDITOS
 FROM CURSOS
 ORDER BY CREDITOS DESC LIMIT 1;
